ebde009dc31592224213727cfcc248cd81b1783c
Ahmed Alfaifi <a.alfaifisa@gmail.com>
2024-11-14 20:01:53 +0300

    Made an endpoint to create a new article

--- blog-api/src/article/article.service.ts
+++ blog-api/src/article/article.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Article } from './entities/article.entity';
 import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
-  }
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>,
+  ) {}
 
-  findAll() {
-    return `This action returns all article`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
-  }
-
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} article`;
+  create(createArticleDto: CreateArticleDto): Promise<Article> {
+    const article = this.articleRepository.create(createArticleDto);
+    return this.articleRepository.save(article);
   }
 }
